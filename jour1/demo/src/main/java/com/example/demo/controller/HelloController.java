9b1c53b843fcd3eaedbe410ca2d6e5a4d77ca579
nyxx <leila.wilde@laplateforme.io>
2025-09-04 17:55:45 +0100

    Add Thymeleaf template

--- jour1/demo/src/main/java/com/example/demo/controller/HelloController.java
+++ jour1/demo/src/main/java/com/example/demo/controller/HelloController.java
@@ -2,14 +2,12 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
 
     @GetMapping("/")
-    @ResponseBody
     public String hello() {
-        return "Hello World";
+        return "hello"; // This will look for a template named "hello.html" in src/main/resources/templates
     }
 }
